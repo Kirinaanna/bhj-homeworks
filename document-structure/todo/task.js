6866a09bc1272dde944065b4e3923396ad0d5b38
anna Kirina <kuzjminovaas@gmail.com>
2025-08-04 17:42:58 +0300

    Простой список дел Готов

---
+++ document-structure/todo/task.js
@@ -0,0 +1,27 @@
+const todoList = document.querySelector('.tasks__list');
+const input = document.querySelector('.tasks__input');
+const form = document.querySelector('.tasks__form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const value = input.value.trim();
+    if (value !== '') {
+        addTask(value);
+        input.value = '';
+    }
+});
+
+function addTask(text) {
+    const task = document.createElement('div');
+    task.className = 'task';
+    task.innerHTML = `
+    <div class="task__title">${text}</div>
+    <a href="#" class="task__remove">&times;</a>
+  `;
+
+    task.querySelector('.task__remove').addEventListener('click', function (event) {
+        event.preventDefault();
+        task.remove();
+    });
+    todoList.appendChild(task);
+}
