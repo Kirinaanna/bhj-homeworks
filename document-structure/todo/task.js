b50123cf7b3a70ad4cbd7f2ad8090f9ea814bcb1
anna Kirina <kuzjminovaas@gmail.com>
2025-08-11 18:45:31 +0300

    Исправление 2задания готово

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,27 +1,29 @@
-const todoList = document.querySelector('.tasks__list');
-const input = document.querySelector('.tasks__input');
-const form = document.querySelector('.tasks__form');
+document.addEventListener('DOMContentLoaded', function () {
+    const todoList = document.querySelector('.tasks__list');
+    const input = document.querySelector('.tasks__input');
+    const form = document.querySelector('.tasks__control');
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    const value = input.value.trim();
-    if (value !== '') {
-        addTask(value);
-        input.value = '';
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const value = input.value.trim();
+        if (value !== '') {
+            addTask(value);
+            input.value = '';
+        }
+    });
+
+    function addTask(text) {
+        const task = document.createElement('div');
+        task.className = 'task';
+        task.innerHTML = `
+        <div class="task__title">${text}</div>
+        <a href="#" class="task__remove">&times;</a>
+      `;
+
+        task.querySelector('.task__remove').addEventListener('click', function (event) {
+            event.preventDefault();
+            task.remove();
+        });
+        todoList.appendChild(task);
     }
 });
-
-function addTask(text) {
-    const task = document.createElement('div');
-    task.className = 'task';
-    task.innerHTML = `
-    <div class="task__title">${text}</div>
-    <a href="#" class="task__remove">&times;</a>
-  `;
-
-    task.querySelector('.task__remove').addEventListener('click', function (event) {
-        event.preventDefault();
-        task.remove();
-    });
-    todoList.appendChild(task);
-}
